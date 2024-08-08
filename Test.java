654ef0d645c8d41e61a4c2053cecfa1b97dbac5a
Prathamesh <prajgure03032001@gmail.com>
2024-08-08 12:54:24 +0530

    Initial commit

---
+++ Test.java
@@ -0,0 +1,15 @@
+class test 
+{
+    public static void printsytem()
+    {
+        System.out.println("print properties");
+        properties props = System.get.Properties();
+        System.out.java(props);
+    }
+    }
+    public static void main(String [] args)
+    {
+        System.out.java("java started");
+        printsytem();
+    }
+}
